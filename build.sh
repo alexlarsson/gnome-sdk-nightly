0da57f00b6942d235edd12f2825ee11fa345b21a
Alexander Larsson <alexl@redhat.com>
2016-01-13 09:51:48 +0100

    build.sh: Use new xdg-app builder features

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-xdg-app-builder app org.gnome.Sdk.json
-cp metadata.sdk app/metadata
+xdg-app-builder --ccache app org.gnome.Sdk.json
+cp metadata.sdk app/metadata.sdk
 mv app/files/manifest.json app/usr/
-xdg-app build-export -r repo app
+xdg-app build-export --metadata=metadata.sdk --exclude="/lib/debug/*" -r repo app

0d9f8be8959f293310efe285d9d2a7c521a3e148
Alexander Larsson <alexl@redhat.com>
2016-01-14 11:18:36 +0100

    Some more cleanups

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 set -e
-set -x
 
-xdg-app-builder --ccache app org.gnome.Sdk.json
-cp metadata.sdk app/metadata.sdk
-mv app/files/manifest.json app/usr/
-xdg-app build-export --metadata=metadata.sdk --exclude="/lib/debug/*" -r repo app
+xdg-app-builder --ccache sdk org.gnome.Sdk.json
+cp metadata.sdk sdk/metadata.sdk
+mv sdk/files/manifest.json sdk/usr/
+xdg-app build-export --metadata=metadata.sdk --exclude="/lib/debug/*" -r repo sdk

d3e28e2cce80bff08df751a0cd371527c7336418
Alexander Larsson <alexl@redhat.com>
2016-01-18 16:53:06 +0100

    Use the xdg-app-builder platform building support

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@
 set -e
 
 rm -rf sdk
-xdg-app-builder --ccache sdk org.gnome.Sdk.json
-cp metadata.sdk sdk/metadata.sdk
-mv sdk/files/manifest.json sdk/usr/
-xdg-app build-export --metadata=metadata.sdk --exclude="/lib/debug/*" -r repo sdk
-
-./build_platform.sh
+xdg-app-builder --ccache --require-changes --repo=repo --subject="Nightly build of org.gnome.Sdk, `date`" ${EXPORT_ARGS-} sdk org.gnome.Sdk.json
